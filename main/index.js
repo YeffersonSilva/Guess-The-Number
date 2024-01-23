60dd4ab7ea895270a0c8cb35cddeb0ccb4577873
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-01-23 15:28:28 -0300

    Primera parte de la funcion init

---
+++ main/index.js
@@ -0,0 +1,10 @@
+
+let computerNumber
+
+
+//Funcion de inicio , numero aleatorio
+function init() {
+    computerNumber = Math.floor(Math.random() * 100) + 1
+    console.log(computerNumber)    
+}
+    
