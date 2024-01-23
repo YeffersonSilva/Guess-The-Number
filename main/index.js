31ebc3cb680b4d7066aad21d43f2930f50211942
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-01-23 15:30:51 -0300

    Arreglando bugs del html

--- main/index.js
+++ main/index.js
@@ -2,7 +2,7 @@
 let computerNumber
 
 
-//Funcion de inicio , numero aleatorio
+//Funcion de inicio , numero aleatorios
 function init() {
     computerNumber = Math.floor(Math.random() * 100) + 1
     console.log(computerNumber)    
